b66ceb56412e65d5ec1b27f467679363d5ac0a80
chrl0t <charlotte_i@live.co.uk>
2021-02-17 15:38:19 +0000

    added text input to create screen page

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,11 +1,16 @@
-import React, { useContext } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useContext, useState } from "react";
+import { View, Text, StyleSheet, TextInput } from "react-native";
 import { Context } from "../context/BlogContext";
 
 const CreateScreen = ({ navigation }) => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
   return (
     <View>
-      <Text>Create</Text>
+      <Text>Title:</Text>
+      <TextInput value={title} onChangeText={(text) => setTitle(text)} />
+      <Text>Content:</Text>
+      <TextInput value={content} onChangeText={(text) => setContent(text)} />
     </View>
   );
 };
